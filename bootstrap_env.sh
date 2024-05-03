1285e8b199da6d68251b97952f5cd6449ea8f5f4
humanoid2050 <humanoid2050@katana>
2024-05-03 19:58:34 -0400

    improvements

--- bootstrap_env.sh
+++ bootstrap_env.sh
@@ -10,10 +10,10 @@ pip install gymnasium torch "tensorflow==2.15" carla wandb "cython<3" mujoco \
 
 
 git clone https://github.com/TTF2050/d4rl.git && cd d4rl && git checkout update-to-gymnasium  && pip install -e .
-
 python -c "import d4rl"
+cd ..
 
 git clone https://github.com/karpathy/minGPT.git && cd minGPT && pip install -e .
-
+cd ..
 
 python -m atari_py.import_roms /Atari-2600-VCS-ROM-Collection 
--- setup.sh
+++ setup.sh
@@ -1,6 +1,31 @@
 #for reference only
 exit 1
 
+#on host/login node
+cd ~
+mkdir dectransform
+cd dectransform
+#read-only clone and checkout
+git clone https://github.com/TTF2050/decision-transformer.git 
+cd decision-transformer
+git checkout oscar
+cd ../..
+
+#build image
 singularity build dectransform.simg docker://ttf2050/dectransform:0.0.1
+#start image
+singularity run --nv --bind dectransform:${HOME}/dectransform dectransform.simg
+#put the env configuration script in the right place
+cd dectransform
+cp decision-transformer/bootstrap_env.sh .
+./bootstrap_env.sh
+# exit image shell ctrl+d
+
+
+# do stuff
 interact -q gpu -g 2
 singularity run --nv --bind dectransform:${HOME}/dectransform dectransform.simg
+cd dectransform/
+source .env/bin/activate
+cd decision-transformer/gym
+./run.sh
